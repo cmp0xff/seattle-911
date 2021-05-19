c74f515482e23830a9f5dc23e18f24a4b26ffb2f
cmp0xff <yfwang@thp.uni-koeln.de>
2021-05-19 15:26:39 +0200

    init.py with tmp and test

--- init.py
+++ init.py
@@ -1,9 +1,21 @@
 
+import os
 import pandas as pd
 
 def init_calls() :
     # Convert to pandas DataFrame
-    calls_df = pd.read_csv('./data/calls.csv')
+    
+    calls_df_pt = './tmp/calls_df.h5'
+    
+    if os.path.isfile(calls_df_pt) :
+        return(pd.read_hdf(calls_df_pt, key='c', mode='r'))
+    
+    calls_pt = './data/calls.csv'
+    
+    if not os.path.exists(calls_pt) :
+        os.system('cat get_calls.sh | sh')
+    
+    calls_df = pd.read_csv(calls_pt)
     # print(calls_df.info())
     # print(calls_df.head(1))
 
@@ -12,28 +24,37 @@ def init_calls() :
     calls_df.dropna(inplace=True)
     calls_df.set_index('Datetime', inplace=True)
     calls_df.sort_index(inplace=True)
+    
+    calls_df.to_hdf(calls_df_pt, key='c', mode='w')
 
     # results_df['type'] = results_df['type'].astype('category')
-
-    # print(calls_df.info())
-    # print(calls_df.head(2))
-    # print(calls_df.tail(2))
     
     return(calls_df)
 
 def init_weather() :
+    
+    wtr_df_pt = './tmp/wtr_df.h5'
+    
+    if os.path.isfile(wtr_df_pt):
+        return(pd.read_hdf(wtr_df_pt, key='w', mode='r'))
+    
     wtr_df = pd.read_csv('./data/Seattle Weatherdata 2002 to 2020.csv')
     wtr_df['datetime'] = pd.to_datetime(wtr_df['dt'], unit='s').\
     dt.tz_localize(tz='UTC').\
     dt.tz_convert('US/Pacific')
     wtr_df.set_index('datetime', inplace=True)
-
-    # wtr_df.info()
-    # wtr_df.head(2)
     
+    wtr_df.to_hdf(wtr_df_pt, key='w', mode='w')
+
     return(wtr_df)
 
 def init() :
+    
+    xy_df_pt = './tmp/xy_df.csv'
+    
+    if os.path.isfile(xy_df_pt) :
+        return(pd.read_csv(xy_df_pt)) #, key='x', mode='r'
+    
     calls_df = init_calls()
     wtr_df = init_weather()
     
@@ -46,7 +67,9 @@ def init() :
     xy_df = xy_df.join(x_raw).dropna()
 
     xy_df.drop_duplicates(inplace=True)
-
-    # print(xy_df.info())
+    
+    print(xy_df.info())
+    
+    xy_df.to_csv(xy_df_pt) #, key='x', mode='w'
 
     return(xy_df)
--- prep.py
+++ prep.py
@@ -5,13 +5,14 @@ from sklearn.model_selection import train_test_split
 import init
 
 def train_test(tsize=.3, rseed=0) :
-    xy_df = init.init().loc['2016':'2020']
+    xy_all = init.init()
+    xy_df = xy_all.loc['2016':'2020']
 
     return train_test_split(
         xy_df.iloc[:, 1:], 
         xy_df.iloc[:, 0], 
         test_size=tsize,
-        random_state=rseed)
+        random_state=rseed) # + (xy_all.loc['2015'].iloc[:, 1:], xy_all.loc['2015'].iloc[:, 0])
 
 from sklearn.pipeline import Pipeline
 import sklearn.preprocessing as skl_prep
